329d3bb8736fe160c3e3eee0697bb0d88203d5c7
cxxxr <g23tlm@gmail.com>
2018-05-07 22:58:11 +0900

    paraphrase

--- window.lisp
+++ window.lisp
@@ -36,10 +36,10 @@
         (xlib:add-to-save-set xwin)
         (xlib:reparent-window xwin frame 0 +frame-height+)
         (set-netwm-allowed-actions xwin)
-        (cond ((eq (xlib:window-map-state xwin) :viewable)
-               (incf (window-count-ignore-unmap window)))
+        (cond ((eq (xlib:window-map-state xwin) :unmapped)
+               (xlib:map-window frame))
               (t
-               (xlib:map-window frame)))))))
+               (incf (window-count-ignore-unmap window))))))))
 
 (defun remove-window (window)
   (xlib:with-server-grabbed ((display *window-manager*))
