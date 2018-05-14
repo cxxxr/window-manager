37d020750c8abb7289a547a98567b38f84f0a766
cxxxr <g23tlm@gmail.com>
2018-05-14 16:21:28 +0900

    use a after method

--- vdesk.lisp
+++ vdesk.lisp
@@ -3,16 +3,22 @@
 (defun vdesk-index (vdesk)
   (position vdesk (vdesks *window-manager*)))
 
+(defmethod (setf vdesks) :after (vdesks (*window-manager* window-manager))
+  (log-format "(setf vdesks) vdesks = ~D" (length (vdesks *window-manager*)))
+  (update-net-number-of-desktops))
+
+(defmethod (setf current-vdesk) :after (vdesk (*window-manager* window-manager))
+  (log-format "(setf current-vdesk) vdesk = ~A" vdesk)
+  (set-net-current-desktop (vdesk-index vdesk)))
+
 (defun add-vdesk ()
   (let ((vdesk (make-instance 'vdesk :screen (screen *window-manager*))))
     (alexandria:nconcf (vdesks *window-manager*)
-                       (list vdesk))
-    (update-net-number-of-desktops)))
+                       (list vdesk))))
 
 (defun change-to-vdesk (new-vdesk)
   (let ((old-vdesk (current-vdesk *window-manager*)))
     (unless (eq new-vdesk old-vdesk)
       (mapc #'show-window (reverse (vdesk-windows new-vdesk)))
       (mapc #'hide-window (vdesk-windows old-vdesk))
-      (setf (current-vdesk *window-manager*) new-vdesk)
-      (set-net-current-desktop (vdesk-index new-vdesk)))))
+      (setf (current-vdesk *window-manager*) new-vdesk))))
